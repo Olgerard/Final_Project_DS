16c2889754f4d24403fd65f2afe63e3cb20d3335
Olgerard <olivier.gerard.2004@hotmail.com>
2026-05-03 15:46:58 +0200

    Firt version of Order class

---
+++ broker/src/main/java/com/example/webcontent/Order.java
@@ -0,0 +1,49 @@
+package com.example.webcontent;
+
+public class Order {
+    private int orderId;
+    private String customerName;
+    private String deliveryAddress;
+    private String paymentInfo;
+    private String status;
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
+
+    public String getPaymentInfo() {
+        return paymentInfo;
+    }
+
+    public void setPaymentInfo(String paymentInfo) {
+        this.paymentInfo = paymentInfo;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
