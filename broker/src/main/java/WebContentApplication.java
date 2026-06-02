e2d0e458b5fd8fbacfbe2a65a6f69f1601f4b023
Phunicha <6522790227@g.siit.tu.ac.th>
2026-06-03 01:43:21 +0200

    fix broker

--- broker/src/main/java/WebContentApplication.java
+++ broker/src/main/java/WebContentApplication.java
@@ -1,16 +1,14 @@
 package broker;
-<<<<<<<< HEAD:broker/src/main/java/BrokerApplication.java
 
-========
->>>>>>>> task1-2pc:broker/src/main/java/WebContentApplication.java
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-public class BrokerApplication {
+@SpringBootApplication(scanBasePackages = {"broker", "controller", "service", "config", "domain"})
+@EnableJpaRepositories(basePackages = "domain")
+public class WebContentApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(broker.BrokerApplication.class, args);
+        SpringApplication.run(WebContentApplication.class, args);
     }
-
 }
